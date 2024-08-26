7c730c9c852698854cf4d9c0ee4d1478fc4bb8b2
Moonish Maredia <moonishmaredia@Moonishs-MBP.attlocal.net>
2024-08-25 22:18:48 -0500

    Allow render site to access api

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    ALLOWED_ORIGINS: list = ["http://localhost:3000", "https://localhost:3000"]
+    ALLOWED_ORIGINS: list = ["http://localhost:3000", "https://localhost:3000", "https://fakekayaksite.onrender.com", "http://fakekayaksite.onrender.com"]
 
     class Config:
         env_file = ".env"
