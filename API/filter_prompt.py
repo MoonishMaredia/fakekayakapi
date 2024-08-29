d9edf14c0b279c35e8b7e2a08e39af81d9e4d739
Moonish Maredia <moonishmaredia@Moonishs-MBP.attlocal.net>
2024-08-29 03:29:37 -0500

    Updates to prompts for more stability and accuracy

--- API/filter_prompt.py
+++ API/filter_prompt.py
@@ -1,4 +1,3 @@
-# prompts/code_prompt.py
 from datetime import datetime
 
 def get_filter_prompt(currentFilters):
@@ -8,7 +7,7 @@ def get_filter_prompt(currentFilters):
     f" Respond with the appropriate function calls in the following format: [functionName1, functionArguments1, functionName2, functionArguments2]."
     f" If no functions need to be called, respond with: []."
     f"\n\n"
-    
+
     # Important Note
     f"Important Note: Focus ONLY on parts of the user's message requesting filters to the existing flight search."
     f" Ignore any additional actions such as updating or sorting."
@@ -29,7 +28,7 @@ def get_filter_prompt(currentFilters):
     f"Item 2: Available Filters\n"
     f"<\n"
     f"stops: Filters flights based on the number of stops. Valid return values are 0 (nonstop), 1 (1 or fewer stops), 2 (2 or fewer stops), 100 (no filter). Examples: [stops, 0], [stops,1], [stops,2], [stops,100]\n"
-    f"airlines: Filters out specific airline carriers. Valid return value is a complete object given in 'airlines' but changing the field to true or false depending on user message. Example: [airlines, '{{American: true, Delta: true, JetBlue: false}}']\n"
+    f"airlines: Filters specific airline carriers. Valid return value is a complete object with all airlines from 'currentFilters.airlines', changing each field to true or false based on the user message. When excluding airlines, set them to false. When including airlines, set them to true and all others to false. Examples: [airlines, '{{American: false, Delta: true, JetBlue: true, United: true, Spirit: true, ...}}']\n"
     f"price: Filters out values above a certain price value. Valid values are integers. Example: [price, 400]\n"
     f"departureTime: Filters out flights that are not in a takeoff time range. Valid value is a 2-item array of integers between 0 and 24 where 0 is 12:00AM and 24 is 12:00PM. Example: [departureTime, [8,14]]\n"
     f"arrivalTime: Filters out flights that do not land in a specific time range. Valid value is a 2-item array of integers between 0 and 24 where 0 is 12:00AM and 24 is 12:00PM. Example: [arrivalTime, [14,20]]\n"
@@ -37,42 +36,29 @@ def get_filter_prompt(currentFilters):
     f"totalDuration: Filters out flights where total travel time is not in a specific time range. Valid value is a 2-item array of integers between 0 and 24 where 0 represents the minimum and 24 represents the maximum travel time in hours. Example: [totalDuration, [0,10]]\n"
     f">\n\n"
 
-     # Important Notes:
-    f"Important Note 1: If the user specifies only value of a filter for filter items such as departureTime, arrivalTime, layoverDuration, totalDuration, use the currentFilters object to fill in the missing values. For example, if the user specifies a maximum layover duration of 4 hours, and no minimum is provided, retain the current minimum layover duration from currentFilters.\n"
-    f"Important Note 2: If the user requests to clear all existing filters, the response should be [CLEARFILTERS, True]. Any subsequent filters specified by the user should be applied after clearing the current filters. Example: [CLEARFILTERS, True, stops, 0].\n\n"
+    # Important Notes:
+    f"Important Note 1: If the user specifies only one value of a filter for filter items such as departureTime, arrivalTime, layoverDuration, totalDuration, use the currentFilters object to fill in the missing values. For example, if the user specifies a maximum layover duration of 4 hours, and no minimum is provided, retain the current minimum layover duration from currentFilters.\n"
+    f"Important Note 2: If the user requests to clear all existing filters, the response should be [CLEARFILTERS, True]. Any subsequent filters specified by the user should be applied after clearing the current filters. Example: [CLEARFILTERS, True, stops, 0].\n"
+    f"Important Note 3: For airline filters, always provide a complete object with all airlines from currentFilters.airlines, updating their values based on the user's request. Do not use ellipsis (...) in the response.\n\n"
 
     # Example Responses
     f"Example Responses:\n"
     f"UserMessage: Only show non-stop flights, YourResponse: [stops, 0]\n"
-    f"UserMessage: Don't show JetBlue or American flights, YourResponse: [airlines, '{{..., JetBlue: false, American: false}}']\n"
+    f"UserMessage: Don't show JetBlue or American flights, YourResponse: [airlines, '{{American: false, Delta: true, JetBlue: false, United: true, Spirit: true, Frontier: true, Alaska: true, \"Multiple Airlines\": true}}']\n"
+    f"UserMessage: Only show Delta and United flights, YourResponse: [airlines, '{{American: false, Delta: true, JetBlue: false, United: true, Spirit: false, Frontier: false, Alaska: false, \"Multiple Airlines\": false}}']\n"
+    f"UserMessage: Exclude all airlines except Southwest, YourResponse: [airlines, '{{American: false, Delta: false, JetBlue: false, United: false, Spirit: false, Frontier: false, Alaska: false, \"Multiple Airlines\": false, Southwest: true}}']\n"
+    f"UserMessage: Include all airlines, YourResponse: [airlines, '{{American: true, Delta: true, JetBlue: true, United: true, Spirit: true, Frontier: true, Alaska: true, \"Multiple Airlines\": true}}']\n"
     f"UserMessage: Only show flights where cost is less than 400, YourResponse: [price, 400]\n"
     f"UserMessage: Don't show flights that takeoff earlier than 10am, YourResponse: [departureTime, [10, 24]]\n"
     f"UserMessage: Don't show flights that land after 6pm, YourResponse: [arrivalTime, [0, 18]]\n"
     f"UserMessage: Don't show flights that have a layover of 3 or more hours, YourResponse: [layoverDuration, [0, 3]]\n"
-    f"UserMessage: Don't show flights that have total travel time of more than 10 hours, YourResponse: [layoverDuration, [0, 10]]\n"
-    f"UserMessage: Only show non-stop flights that are not Spirit Airlines and cost less than 300, YourResponse: [stops, 0, airlines, '{{...,Spirit:false}}', price:300]\n"
+    f"UserMessage: Don't show flights that have total travel time of more than 10 hours, YourResponse: [totalDuration, [0, 10]]\n"
+    f"UserMessage: Only show non-stop flights that are not Spirit Airlines and cost less than 300, YourResponse: [stops, 0, airlines, '{{American: true, Delta: true, JetBlue: true, United: true, Spirit: false, Frontier: true, Alaska: true, \"Multiple Airlines\": true}}', price, 300]\n"
     f"UserMessage: Only show flights that takeoff before 10am and land after 5pm, YourResponse: [departureTime, [0,10], arrivalTime, [17,24]]\n"
-    f"UserMessage: Clear the filters and add filter to only show flights less than $500, YourResponse: [CLEARFILTERS, True, stops, 0]\n"
+    f"UserMessage: Clear the filters and add filter to only show flights less than $500, YourResponse: [CLEARFILTERS, True, price, 500]\n"
     f"User Message: Update the destination airport to LaGuardia, YourResponse: []\n"
-    f"User Message: Sort flights based on Shortest Duration, YourResponse: []\n"
+    f"User Message: Sort flights based on Shortest Duration, YourResponse: []\n\n"
 
-    # Example Responses:
-    f"Example Responses:\n"
-    f"UserMessage: Only show non-stop flights. YourResponse: [stops, 0]\n"
-    f"UserMessage: Don't show JetBlue or American flights. YourResponse: [airlines, '{{..., JetBlue: false, American: false}}']\n"
-    f"UserMessage: Only show flights where cost is less than 400. YourResponse: [price, 400]\n"
-    f"UserMessage: Don't show flights that take off earlier than 10 AM. YourResponse: [departureTime, [10, 24]]\n"
-    f"UserMessage: Don't show flights that land after 6 PM. YourResponse: [arrivalTime, [0, 18]]\n"
-    f"UserMessage: Don't show flights that have a layover of 3 or more hours. YourResponse: [layoverDuration, [0, 3]]\n"
-    f"UserMessage: Don't show flights that have total travel time of more than 10 hours. YourResponse: [totalDuration, [0, 10]]\n"
-    f"UserMessage: Only show non-stop flights that are not Spirit Airlines and cost less than 300. YourResponse: [stops, 0, airlines, '{{...,Spirit:false}}', price, 300]\n"
-    f"UserMessage: Only show flights that take off before 10 AM and land after 5 PM. YourResponse: [departureTime, [0,10], arrivalTime, [17,24]]\n"
-    f"UserMessage: Clear applied filters. YourResponse: [CLEARFILTERS, True]\n"
-    f"UserMessage: Clear the filters and add filter to only show flights less than $500. YourResponse: [CLEARFILTERS, True, price, 500]\n"
-    f"User Message: Update the destination airport to LaGuardia. YourResponse: []\n"
-    f"User Message: Sort flights based on Shortest Duration. YourResponse: []\n\n"
-
-    
     #Ambiguous Situations
     f"If it is unclear what filter or filter value should be applied, return []"
     )
